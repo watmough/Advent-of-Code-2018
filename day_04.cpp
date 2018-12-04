613470439d7a8096b9e3fad935f910b6f857b363
Jonathan Watmough <jonathan@jonathans-imac.attlocal.net>
2018-12-04 01:04:21 -0600

    Day 04 cleanup

--- day_04.cpp
+++ day_04.cpp
@@ -8,8 +8,7 @@ struct is_delim : std::unary_function<const char&, bool> {
     bool operator()(const char& c) const { return strchr(" :[]#-",int(c))!=nullptr; }
 };
 
-const static bool compare(const pair<int,int>& a, const pair<int,int>& b)
-{
+const static bool compare(const pair<int,int>& a, const pair<int,int>& b) {
     return (a.second<b.second);
 }
 
@@ -19,9 +18,7 @@ int main(int argc, char* argv[])
     sort(begin(input),end(input));
 
     // build a map of guards to total sleep time and by minute asleep
-    auto guard{0};
-    auto wakeat{0};
-    auto sleepat{59};
+    auto guard{0}, sleepat{59}, wakeat{0};
     auto sleeping = map<int,int>();
     auto minutes = map<int,array<int,60>>();
     for (auto l : input) {
@@ -30,34 +27,27 @@ int main(int argc, char* argv[])
         if (l.find("asleep")!=string::npos) sleepat = stoi(tokens[5]);
         if (l.find("wake")) {
             wakeat = stoi(tokens[5]);
-            cout << "Guard " << guard << " sleep " << sleepat << " wake " << wakeat << "\n";
             sleeping[guard]+=wakeat-sleepat;
             for (int i=sleepat;i<wakeat;++i) minutes[guard][i]++;
         }
     }
 
     // Part 1 - Find the sleepiest guard * the minute he spent most asleep
-    auto g = max_element(begin(sleeping),end(sleeping),compare);
-    cout << "Sleepiest guard: " << (*g).first << " slept " << (*g).second << " mins.\n";
-    guard = (*g).first;
-    auto m = max_element(begin(minutes[guard]),end(minutes[guard]));
-    auto minute = m-minutes[guard].data();
+    guard = max_element(begin(sleeping),end(sleeping),compare).operator*().first;
+    auto minute = max_element(begin(minutes[guard]),end(minutes[guard]))-minutes[guard].data();
     cout << "Part 1: Guard * Minutes: " << guard*minute << "\n";
 
-    auto maxminutes = 0;
-    auto mostminute = 0;
-    auto mostguard = 0;
+    // Part 2 - Find the Guard# * Most Slept in Minute
+    auto maxminutes{0},mostminute{0},mostguard{0};
     for( auto g : minutes ) {
-        guard = g.first;
-
-        auto m = max_element(begin(minutes[guard]),end(minutes[guard]));
-        auto minute = m-minutes[guard].data();
-        cout << "g: " << guard << " Sleepiest minute " << minute << " for " << *m << "\n";
+        // find the minute for this guard
+        auto m = max_element(begin(minutes[g.first]),end(minutes[g.first]));
+        auto minute = m-minutes[g.first].data();
 
         if (*m>maxminutes) {
             maxminutes = *m;
             mostminute = minute;
-            mostguard = guard;
+            mostguard = g.first;
         }
     }
     cout << "Part 2: Most frequently asleep guard * minute: " << mostguard << " m " << mostminute << " -> " << mostguard*mostminute << "\n";
