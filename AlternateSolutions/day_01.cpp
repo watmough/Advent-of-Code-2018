8216879abcf5d12414256291aad74ee51ba2ba5d
Jonathan Watmough <jonathan@jonathans-imac.attlocal.net>
2018-12-04 22:18:10 -0600

    Cleanup

--- AlternateSolutions/day_01.cpp
+++ AlternateSolutions/day_01.cpp
@@ -7,28 +7,22 @@ using namespace std;
 int main() 
 {
     // read input
-    auto lines = vector<string>(read_input("day_01.txt"));
+    auto changes = read_ints("day_01.txt");
 
-    // Part 1 - parse input lines to numbers and total
-    auto freq = int64_t(0);
-    auto values = vector<int64_t>();
-    transform(begin(lines),end(lines),back_inserter(values),
-              [&](string s) -> int64_t {int64_t val=stoi(s); freq+=val; return val;});
-    cout << "Part 1: " << freq << endl;
+    // Part 1 - calculate final frequency
+    cout << "Part 1: " << accumulate(begin(changes),end(changes),0l) << endl;
 
     // Part 2 - change frequencies until we see a duplicate frequency
     //          freq needs to be reset so we catch first repeated freq
-    freq = 0;
-    auto freqs = set<int64_t>();
-    while (true) {
-        transform(begin(values),end(values),inserter(freqs,end(freqs)),
-                    [&](int64_t v) -> int64_t {
-                        if (freqs.count((freq+=v))>0) {
-                            cout << "Part 2: " << freq << "\n";
-                            exit(0);
-                        }
+    auto freq{0l};
+    auto part2complete{false};
+    auto freqs{unordered_set<int64_t>()};
+    while (!part2complete) {
+        transform(begin(changes),end(changes),inserter(freqs,begin(freqs)),
+                    [&](const int64_t& c) -> int64_t {
+                        if (freqs.count((freq+=c))>0 && !part2complete) 
+                            cout << "Part 2: " << freq << "\n", part2complete = true;
                         return freq;
                     });
     }
 }
-
