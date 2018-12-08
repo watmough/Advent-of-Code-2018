b964a4e4d4826f1e12da17f9554c545767eac933
Jonathan Watmough <jonathan@jonathans-imac.attlocal.net>
2018-12-07 20:37:27 -0600

    Day 07 - The Sum of Its Parts

--- day_07.cpp
+++ day_07.cpp
@@ -7,9 +7,13 @@ using namespace std;
 #if 1
     #define FILENAME "day_07.txt"
     #define COUNT 101
+    #define WORKERS 5
+    #define TIME 61
 #else
     #define FILENAME "day_07_tst.txt"
     #define COUNT 7
+    #define WORKERS 2
+    #define TIME 1
 #endif
 
 set<char> available(set<char> chars, multimap<char,char> has_dep)
@@ -50,6 +54,108 @@ multimap<char,char> done_step(char c, multimap<char,char> has_dep)
 //     return has_dep;
 // }
 
+// int main()
+// {
+//     auto chars = set<char>{};
+//     auto has_dep = multimap<char,char>{};
+
+//     // Step T must be finished before step W can begin.
+//     // read data
+//     FILE *f = fopen(FILENAME,"rb");
+//     char fs,ss;
+//     while (!feof(f) && fscanf(f,"Step %c must be finished before step %c can begin.\n",&fs,&ss)) {
+//         printf("%c -> %c\n",fs,ss);
+//         chars.insert(fs);
+//         chars.insert(ss);
+//         has_dep.insert(make_pair(ss,fs));
+//     }
+//     fclose(f);
+
+//     // transitive deps
+// //    has_dep = get_transitive_deps(chars,has_dep);
+
+//     auto order = string{""};
+
+//     // for each step,
+//     while (chars.size()>0) {
+//         // candidates w/ no dependencies
+//         auto candidates = available(chars,has_dep);
+
+//         // do first only
+//         auto c = *(begin(candidates));
+//         order.push_back(c);
+
+//         // clean up dependencies
+//         has_dep = done_step(c, has_dep);
+//         chars.erase(c);
+
+//         // print order so far
+//         cout << order << "\n";
+//     }
+
+//     printf("Part 1: ...: \n");
+
+//     printf("Part 2: ...: \n");
+//     return 0;
+// }
+
+
+// work_queue
+// initialize with a count of workers, at time 0
+// call assign to assign a job to complete
+// 
+class work_queue
+{
+public:
+    work_queue(int workers) : tasks(), workers(workers), thetime{0} {};
+
+    bool inflight(char c) {
+        return any_of(begin(tasks),end(tasks),[&](const auto& t)->bool{return t.first==c;});
+    }
+
+    int gettime() {
+        return thetime;
+    };
+
+    int tick() {    // tick to the soonest available job, returning the time (may be >1 job)
+        cout << "tick\n";
+        assert(haswork());
+        std::stable_sort(begin(tasks),end(tasks),[&](const auto& l,const auto& r){return l.second<r.second;});
+        return (thetime=tasks.front().second);
+    };
+
+    vector<pair<char,int>> completed()
+    {
+        cout << "completed()\n";
+        auto done = vector<pair<char,int>>{};
+        copy_if(begin(tasks),end(tasks),std::back_inserter(done),[&](const auto& t)->bool{return t.second==thetime;});
+        for_each(begin(done),end(done),[&](const auto& t){tasks.erase(tasks.begin());});
+        return done;
+    }
+
+    bool haswork() {
+        return !tasks.empty();
+    }
+
+    bool busy() {
+        return tasks.size()==workers;
+    };
+
+    pair<char,int> work(char c) {        // assign and return task,eta for this job
+        cout << "Work: " << c << " " << thetime+TIME+c-'A' << "\n";
+        assert(!busy());                // must have at least one elf free
+        auto t = make_pair(c,thetime+/* 61 or 1 */TIME+c-'A');
+        tasks.push_back(t);
+        return t;
+    };
+private:
+    vector<pair<char,int>> tasks;
+    int workers;
+    int thetime;
+};
+
+
+// part 2
 int main()
 {
     auto chars = set<char>{};
@@ -67,31 +173,45 @@ int main()
     }
     fclose(f);
 
-    // transitive deps
-//    has_dep = get_transitive_deps(chars,has_dep);
+    // create a work queue
+    work_queue w(WORKERS);
 
-    auto order = string{""};
+    // get candidates for next worker
+    auto candidates = available(chars,has_dep);
 
     // for each step,
     while (chars.size()>0) {
-        // candidates w/ no dependencies
-        auto candidates = available(chars,has_dep);
+        
+        // assign a worker
+        while (candidates.size() && !w.busy()) {
+            auto c = *(begin(candidates)); 
+            if (!w.inflight(c))
+                w.work(c);
+            candidates.erase(c);
+        }
 
-        // do first only
-        auto c = *(begin(candidates));
-        order.push_back(c);
+        // if busy, clock the next job to completion
+        if (w.busy() || candidates.size()==0) {
+            // clock
+            w.tick();
 
-        // clean up dependencies
-        has_dep = done_step(c, has_dep);
-        chars.erase(c);
+            // job order
+            auto done = w.completed();
+            for (auto completed : done) {
+                cout << "Completed: " << completed.first << " at time: " << completed.second << "\n";
 
-        // print order so far
-        cout << order << "\n";
+                // completed a task
+                has_dep = done_step(completed.first, has_dep);
+                chars.erase(completed.first);
+
+                // refresh list of candidates 
+                // ### No Clang warning for having an auto on the next line ###
+                candidates = available(chars,has_dep);
+            }
+        }
     }
+    printf("Part 2: Finished at time: %d\n",w.gettime());
 
-    printf("Part 1: ...: \n");
-
-    printf("Part 2: ...: \n");
     return 0;
 }
 
