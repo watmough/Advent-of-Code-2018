139a1ea995b6a22de1c97147cae3ee1ac7ad3646
Jonathan Watmough <jonathan@jonathans-imac.attlocal.net>
2018-12-16 13:01:01 -0600

    Day 14 - Chocolate Charts

--- day_14.cpp
+++ day_14.cpp
@@ -4,37 +4,42 @@
 #include "aoc.hpp"
 using namespace std;
 
-//#define PUZZLE_INPUT "37"           // test input
-#define PUZZLE_INPUT "320851"     // puzzle input
+#define PUZZLE_INPUT "320851"
 
 int main()
 {
     auto elves{map<int,int>()};
-    auto recipes{vector<int>()};
-    for (auto ch : string(PUZZLE_INPUT)) 
-        recipes.push_back(ch-'0');
+    auto recipes{vector<int>{3,7}};     // dammit! you stick with the example initial recipes
+    auto digits{vector<int>{3,2,0,8,5,1}};
+//    auto digits{vector<int>{5,9,4,1,4}};    // test input yielding 2018
     // set elf start points
     for (auto elf{0};elf<2;++elf)
         elves[elf] = elf;
+    auto part1{true};
     while (1) {
         // combine recipes  by summing digits
         auto sum=accumulate(begin(elves),end(elves),0,[&](const auto& t,const auto& e){return t+recipes[e.second];});
         // add recipe digits
         auto sumd=to_string(sum);
-        cout << "New recipe is " << sumd << "\n";
         for_each(begin(sumd),end(sumd),[&](const auto& d){recipes.push_back(d-'0');});
-        for_each(begin(sumd),end(sumd),[&](const auto& d){part1.push_back(d-'0');});
         // step elves forward 1+current recipe, wrapping to new length of recipe list
         for (auto elf{0};elf<2;++elf) {
             elves[elf] = (elves[elf]+1+recipes[elves[elf]]) % recipes.size();
-            cout << "elf " << elf << " -> pos " << elves[elf] << "\n";
         }
         // Look for 10 digits
-        if (recipes.size()>=stoi(PUZZLE_INPUT)+10) {
+        if (part1&&recipes.size()>=stoi(PUZZLE_INPUT)+10) {
             cout << "Part 1: Added 10 digits: "; 
             for_each( begin(recipes)+stoi(PUZZLE_INPUT),end(recipes),[](const auto& d){cout << d << " ";});
             cout << "\n";
-            break;
+            part1=false;
+        }
+        // look for puzzle input in output string
+        if (recipes.size()>digits.size()) {
+            auto it=end(recipes)-digits.size()-1;
+            if (equal(begin(digits),end(digits),it)||equal(begin(digits),end(digits),++it)) {
+                cout << "Part 2: To the left there are " << distance(begin(recipes),it) << "\n";
+                break;
+            }
         }
     }
 }
